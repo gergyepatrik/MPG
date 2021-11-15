b52b72448255c54a36e1bf3399490b830ca64745
Gergye Patrik <medievil98@gmail.com>
2021-11-15 22:58:11 +0100

    Implemented interface controller navigation

---
+++ MPG WatchKit Extension/PortraitInterfaceController.swift
@@ -0,0 +1,37 @@
+//
+//  PortraitInterfaceController.swift
+//  MPG WatchKit Extension
+//
+//  Created by Patrik Gergye on 2021. 11. 15..
+//
+
+import Foundation
+import WatchKit
+
+
+
+class PortraitInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+    
+    
+    //First screen button taps
+    @IBAction func OoutdoorsButtonTapped(){
+        self.pushController(withName: "Outdoors", context: nil)
+    }
+
+    
+    
+    
+
+}
