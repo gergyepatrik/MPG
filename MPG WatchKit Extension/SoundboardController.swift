1f1eb38b506787fed36b3e818c6cc450d101c0e7
Gergye Patrik <medievil98@gmail.com>
2022-03-21 13:59:10 +0100

    Added Soundboard page that can play audio with button presses

---
+++ MPG WatchKit Extension/SoundboardController.swift
@@ -0,0 +1,61 @@
+//
+//  SoundboardController.swift
+//  MPG WatchKit Extension
+//
+//  Created by Patrik Gergye on 2022. 03. 21..
+//
+
+import WatchKit
+import AVFoundation
+import Foundation
+
+
+class SoundboardController: WKInterfaceController, SoundPlaying {
+    
+    var audioPlayer: AVAudioPlayer?
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    @IBAction func playSoundNoice() {
+        playSound(named: "Noice")
+    }
+    
+    @IBAction func playSoundTrain() {
+        playSound(named: "Train")
+    }
+    
+    @IBAction func playSoundTroll() {
+        playSound(named: "Troll")
+    }
+    
+    @IBAction func playSoundBaby() {
+        playSound(named: "BabyCrying")
+    }
+    
+    @IBAction func playSoundWhistle() {
+        playSound(named: "Whistle")
+    }
+    
+    @IBAction func playSoundLaugh() {
+        playSound(named: "Laugh")
+    }
+    
+    @IBAction func playSoundChina() {
+        playSound(named: "China")
+    }
+    
+}
