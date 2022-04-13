39e4b644a04cb0cb287bc6a24ab188ec7e6c1532
Gergye Patrik <medievil98@gmail.com>
2022-04-14 01:23:02 +0200

    Added notification system(have to be updated with dates and content)

--- MPG WatchKit Extension/NotificationController.swift
+++ MPG WatchKit Extension/NotificationController.swift
@@ -16,6 +16,37 @@ class NotificationController: WKUserNotificationInterfaceController {
         super.init()
         
         // Configure interface objects here.
+        
+        // Setting the content of the notification
+        let content = UNMutableNotificationContent()
+        content.title="Daily motivation"
+        content.body="Every day at 7am"
+        
+        // Configuring the recurring date
+        var dateComponents = DateComponents()
+        dateComponents.calendar=Calendar.current
+        
+        // Setting the day to tuesday, in Swift the week doesn't start on monday
+        dateComponents.weekday = 3
+        // Setting the hour
+        dateComponents.hour = 14
+        
+        // Create the trigger as a repeating event
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        // Create the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // Schedule the request with the system
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) {(error) in if error != nil {
+            // Handle errors
+            print("Something went wrong with the notification!!!!!")
+            
+        }}
+        
+        
     }
 
     override func willActivate() {
