7527d045e1c54844116f4f44500a544e52c80f1f
Gergye Patrik <medievil98@gmail.com>
2022-04-17 18:08:02 +0200

    Updated notification timing to work everyday in the morning and at night

--- MPG WatchKit Extension/NotificationController.swift
+++ MPG WatchKit Extension/NotificationController.swift
@@ -24,34 +24,7 @@ class NotificationController: WKUserNotificationInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         
-        // Setting the content of the notification
-        let content = UNMutableNotificationContent()
-        content.title="Daily motivation"
-        content.body="Every day at 7am"
         
-        // Configuring the recurring date
-        var dateComponents = DateComponents()
-        dateComponents.calendar=Calendar.current
-        
-      
-        // Setting the hour to 7am
-        dateComponents.hour = 14
-        dateComponents.minute = 14
-        
-        // Create the trigger as a repeating event
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-        
-        // Create the request
-        let uuidString = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
-        
-        // Schedule the request with the system
-        let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.add(request) {(error) in if error != nil {
-            // Handle errors
-            print("Something went wrong with the notification!!!!!")
-            
-        }}
     }
 
     override func didDeactivate() {
@@ -62,6 +35,71 @@ class NotificationController: WKUserNotificationInterfaceController {
         // This method is called when a notification needs to be presented.
         // Implement it if you use a dynamic notification interface.
         // Populate your dynamic notification interface as quickly as possible.
+        
+        
+        
+        
+        
+        
+        // Setting the content of morning notifications
+        let morningContent = UNMutableNotificationContent()
+        morningContent.title = "Daily motivation"
+        morningContent.body = "Every day at 7am"
+        
+        // Setting the content of night notifications
+        let nightContent = UNMutableNotificationContent()
+        nightContent.title = "Time to chill"
+        nightContent.body = "Every day at 6pm"
+        
+        // Configuring the recurring morning date
+        var morningDateComponents = DateComponents()
+        morningDateComponents.calendar=Calendar.current
+        
+        // Configuring the recurring night date
+        var nightDateComponents = DateComponents()
+        nightDateComponents.calendar=Calendar.current
+        
+        // Setting the morning hour to 7am
+        morningDateComponents.hour = 7
+        morningDateComponents.minute = 00
+        
+        // Setting the night hour to 6pm
+        nightDateComponents.hour = 18
+        nightDateComponents.minute = 00
+        
+        // Create the trigger as a repeating event for mornings
+        let morningTrigger = UNCalendarNotificationTrigger(dateMatching: morningDateComponents, repeats: true)
+        
+        // Create the trigger as a repeating event for nights
+        let nightTrigger = UNCalendarNotificationTrigger(dateMatching: nightDateComponents, repeats: true)
+               
+        // Create the request for mornings
+        let uuidMorningString = UUID().uuidString
+        let morningRequest = UNNotificationRequest(identifier: uuidMorningString, content: morningContent, trigger: morningTrigger)
+        
+        // Create the request for nights
+        let uuidNightString = UUID().uuidString
+        let nightRequest = UNNotificationRequest(identifier: uuidNightString, content: nightContent, trigger: nightTrigger)
+        
+        // Schedule the request with the system for the morning notifications
+        let morningNotificationCenter = UNUserNotificationCenter.current()
+        morningNotificationCenter.add(morningRequest) {(error) in if error != nil {
+            // Handle errors
+            print("Something went wrong with the morning notification!!!!!")
+            
+        }}
+        
+        // Schedule the request with the system for the night notifications
+        let nightNotificationCenter = UNUserNotificationCenter.current()
+        nightNotificationCenter.add(nightRequest) {(error) in if error != nil {
+            // Handle errors
+            print("Something went wrong with the morning notification!!!!!")
+            
+        }}
+        
+        
+        
+        
         
     }
     
