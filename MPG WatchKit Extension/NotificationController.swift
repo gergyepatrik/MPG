00b69d83712573883168c01a26cc259c0a0decdc
Gergye Patrik <medievil98@gmail.com>
2022-04-17 20:07:48 +0200

    Notification complete setup

--- MPG WatchKit Extension/NotificationController.swift
+++ MPG WatchKit Extension/NotificationController.swift
@@ -37,6 +37,31 @@ class NotificationController: WKUserNotificationInterfaceController {
         // Populate your dynamic notification interface as quickly as possible.
         
         
+        func getRandom(inputString: String) -> String{
+        // Initializing empty string array
+        var allLines:[String] = []
+        // Putting the lines of the text file into the elements of the array
+        if let startWordsPath = Bundle.main.path(forResource: inputString, ofType: "txt")
+                {
+                    if let startWords = try? String(contentsOfFile: startWordsPath)
+                    {
+                        allLines = startWords.components(separatedBy: "\n")
+                        print ("allLines count = \(allLines.count)")
+                    }
+                    else
+                    {
+                        allLines = ["error"]
+                    }
+                }
+        // Xcode adds an extra line after the last line
+        // Filtering for empty lines
+        allLines = allLines.filter({ $0 != ""})
+        // Choosing a random line from the collection
+        let randomElement = allLines.randomElement()!
+        // Setting the variable to the random chosen value
+        let morningContentGenerated = randomElement
+        return morningContentGenerated
+        }
         
         
         
@@ -44,12 +69,13 @@ class NotificationController: WKUserNotificationInterfaceController {
         // Setting the content of morning notifications
         let morningContent = UNMutableNotificationContent()
         morningContent.title = "Daily motivation"
-        morningContent.body = "Every day at 7am"
+        morningContent.body = getRandom(inputString: "morningcontent")
+        
         
         // Setting the content of night notifications
         let nightContent = UNMutableNotificationContent()
         nightContent.title = "Time to chill"
-        nightContent.body = "Every day at 6pm"
+        nightContent.body = getRandom(inputString: "nightcontent")
         
         // Configuring the recurring morning date
         var morningDateComponents = DateComponents()
@@ -60,12 +86,12 @@ class NotificationController: WKUserNotificationInterfaceController {
         nightDateComponents.calendar=Calendar.current
         
         // Setting the morning hour to 7am
-        morningDateComponents.hour = 7
-        morningDateComponents.minute = 00
+        morningDateComponents.hour = 18
+        morningDateComponents.minute = 49
         
         // Setting the night hour to 6pm
         nightDateComponents.hour = 18
-        nightDateComponents.minute = 00
+        nightDateComponents.minute = 50
         
         // Create the trigger as a repeating event for mornings
         let morningTrigger = UNCalendarNotificationTrigger(dateMatching: morningDateComponents, repeats: true)
